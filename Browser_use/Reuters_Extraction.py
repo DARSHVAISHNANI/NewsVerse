9319c0bf1d3005b8f35f0310d09b951d94773549
DARSHVAISHNANI <darshvaishnani1234@gmail.com>
2025-07-25 18:32:38 +0530

    Browser_use Application

--- Browser_use/Reuters_Extraction.py
+++ Browser_use/Reuters_Extraction.py
@@ -1,15 +1,35 @@
 import asyncio
+import json
+import os
 from dotenv import load_dotenv
+from browser_use import *
+from browser_use.llm import ChatGoogle
+from pydantic import BaseModel
+from typing import List, Optional
+
 load_dotenv()
-from browser_use import Agent
-from browser_use.llm import ChatGroq, ChatOpenRouter
+
 
 async def main():
+    """
+    Initializes an agent to extract structured data from a Reuters article,
+    runs the agent, prints the result, and saves it to a JSON file.
+    """
     agent = Agent(
-        task="""Your task is to extract the latest news article url from this website: "https://www.reuters.com/" """,
-        llm=ChatOpenRouter(model="moonshotai/kimi-k2:free", 
-                           api_key="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", base_url="https://openrouter.ai/api/v1"),
+        task="""
+        Step-1: Got to this URL: https://www.reuters.com/world/asia-pacific/thailand-rejects-international-mediation-end-fighting-with-cambodia-2025-07-25/
+        
+        Step-2: Wait until it open the website
+        Step-3: Extract the title of the news article
+        Step-4: Extract the full description/HTML of the news article
+        Step-5: Extract the url, date and time of the news article
+        Step-6: Extract the source of the news article
+        Step-7: Store this output in a JSON file.""",
+        llm=ChatGoogle(model="gemini-1.5-flash",
+                       api_key=os.getenv("GROQ_API_KEY"))
     )
-    await agent.run()
 
-asyncio.run(main())
+    history = await agent.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
